89327b5c00c01ee6148b0a594a81f707df9d88d5
sim30n40 <sim30n40@gmail.com>
2015-01-24 13:37:50 +0200

    test

---
+++ scripts/menu.js
@@ -0,0 +1,26 @@
+$( "#menubar" ).buttonset().find("label").css("width","12.4825%");
+$("#home").click(function(){
+	window.location = "main.html";
+});
+$("#movies").click(function(){
+	window.location = "movie.html";
+});
+$("#tv").click(function(){
+	window.location = "tv.html";
+});
+$("#novels").click(function(){
+	window.location = "novel.html";
+});
+$("#games").click(function(){
+	window.location = "game.html";
+});
+$("#characters").click(function(){
+	window.location = "char.html";
+});
+$("#about").click(function(){
+	window.location = "about.html";
+});
+
+$("#contactus").click(function(){
+	window.location = "contactus.html";
+});
